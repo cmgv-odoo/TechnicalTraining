504f2c7dcb70aa80a7b393b8672188ea26c60b37
Carlos Manuel González Vallejo <carlosmanuelgonzalezvallejo@MacBook-Air-de-Carlos.local>
2023-07-19 14:14:51 -0600

    First 1-8 TE

---
+++ carlosapp/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name':'Motorcycle Registry',
+    'summary':""" Manage Registration of Motorcycles """,
+    'description':""" Motorcycle Registry
+    ====================
+    This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.
+       """,
+    'license':'OPL-1',
+    'author':'cmgv-odoo',
+    'website':'www.odoo.com',
+    'category':'Kawiil/Admin',
+    'depends':['base'],
+    'data':[
+        'security/carlosapp_groups.xml',
+        'security/ir.model.access.csv',
+        'security/carlosapp_security.xml',
+        'views/carlosapp_menuitems.xml',
+        'views/carlosapp_views.xml',
+    ],
+    'demo':[
+        'demo/registry_demo.xml',
+    ],
+    'application': True,
+}
