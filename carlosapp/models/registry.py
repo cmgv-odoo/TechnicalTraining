d7317c723fda58e03e7483b0335ebb968781044f
Carlos Manuel Gonzalez Vallejo <carlosmanuelgonzalezvallejo@MacBook-Air-de-Carlos.local>
2023-07-20 12:12:52 -0600

    TE 09 FINAL PART

--- carlosapp/models/registry.py
+++ carlosapp/models/registry.py
@@ -1,4 +1,6 @@
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+import re
 
 class Registry(models.Model):
     _name = 'carlosapp.registry'
@@ -17,8 +19,21 @@ class Registry(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
+        print("here")
         for vals in vals_list:
             if vals.get('name',('MRN0000')) == ('MRN0000'):
                 vals['name'] = self.env['ir.sequence'].next_by_code('registry.number')
         return super().create(vals_list)
 
+    @api.constrains('vin')
+    def _check_vin(self):
+        for registry in self:
+            if re.match(r'^[A-Z]{4}[0-9]{2}[0-9A-Z]{2}[0-9]{6}$', registry.vin) is None:
+                raise ValidationError("It must follow the next pattern Make - 2 Capital Letters \nModel - 2 Capital Letters \nYear - 2 Digits \nBattery Capacity - 2 Capital Letters or Numbers \nSerial Number - 6 Digits")
+
+
+    @api.constrains('license_plate')
+    def _check_license(self):
+        for registry in self:
+            if (re.match(r'[A-Z][A-Z]?[A-Z]?[A-Z]?[0-9][0-9]?[0-9]?[A-Z]?[A-Z]?', registry.license_plate) is None):
+                raise ValidationError("1 - 4 Capital Letters \n1 - 3 Digits \nOptional 2 Capital Letters")
