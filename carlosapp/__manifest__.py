aef1771e9d94764a3e82da2d9e327d4663c2da4a
Carlos Manuel Gonzalez Vallejo <carlosmanuelgonzalezvallejo@MacBook-Air-de-Carlos.local>
2023-07-21 09:50:18 -0600

    TE 11

--- carlosapp/__manifest__.py
+++ carlosapp/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name':'Motorcycle Registry',
-    'summary':""" Manage Registration of Motorcycles """,
+    'summary':""" Manage Registration of s """,
     'description':""" Motorcycle Registry
     ====================
     This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.
@@ -9,7 +9,7 @@
     'author':'cmgv-odoo',
     'website':'www.odoo.com',
     'category':'Kawiil/Admin',
-    'depends':['base'],
+    'depends':['stock'],
     'data':[
         'security/carlosapp_groups.xml',
         'security/ir.model.access.csv',
@@ -17,6 +17,7 @@
         'data/registry_data.xml',
         'views/carlosapp_menuitems.xml',
         'views/carlosapp_views.xml',
+        'views/product_view_inherit.xml',
     ],
     'demo':[
         'demo/registry_demo.xml',
---
+++ carlosapp/models/motorcycle.py
@@ -0,0 +1,30 @@
+from odoo import models, fields, api
+
+class Motorcycle(models.Model):
+    _inherit = "product.template"
+
+
+    horsepower = fields.Char()
+    top_speed = fields.Char()
+    torque = fields.Char()
+    battery_capacity = fields.Char()
+    charge_time = fields.Char()
+    range = fields.Char()
+    curb_weight = fields.Char()
+    make = fields.Char()
+    model = fields.Char()
+    year = fields.Char()
+
+    detailed_type = fields.Selection(selection_add=[
+    ('motorcycle', 'Motorcycle'),
+    ], ondelete={'motorcycle': 'set product'})
+    
+    type = fields.Selection(selection_add=[
+    ('motorcycle', 'Motorcycle'),
+    ], ondelete={'motorcycle': 'set product'})
+
+    
+    def _detailed_type_mapping(self):
+        type_mapping = super()._detailed_type_mapping()
+        type_mapping['motorcycle'] = 'product'
+        return type_mapping
